f156f41e309f8a14f2601c32b80c2da93188c70b
Joinsodome <kangzexi@yeah.net>
2022-05-02 14:07:34 +0800

    Version 0.3.Use logging.

--- frame.py
+++ frame.py
@@ -1,12 +1,16 @@
 import os
 import shutil
+import sys
 import zipfile
 from tempfile import TemporaryDirectory as t_dir
+import logging
 import psutil
-import sys
+
+logging.basicConfig(level=logging.INFO)
+
 
 class Framework:
-    def __init__(self,docpath,zip_here):
+    def __init__(self, docpath, zip_here):
         self.docpath = docpath
         self.user_path = []
         self.user = []
@@ -14,11 +18,11 @@ class Framework:
         self.temp_path = self.temp.name
         self.zip_here = zip_here
         self.rubbish_doc = {
-            'Rubbish':[
+            'Rubbish': [
                 'Backup',
                 'BackupFiles',
                 'ResUpdateV2'],
-            'Documents':[
+            'Documents': [
                 'FileStorage',
                 'Msg',
                 'config'
@@ -27,17 +31,19 @@ class Framework:
         self.app_path = os.getcwd()
 
     def run(self):
-        print('正在做一个检查……')
+        logging.info("Checking......")
         self.check()
-        print('检查成功，正在导入用户列表……')
+        logging.info("Take path......")
         self.take_path()
 
     def check(self):
         pid = psutil.pids()
-        for p in pid:
-            if 'Wechat' in psutil.Process(p).name():
-                print('请关闭微信后重试！')
-                sys.exit(1)
+        try:
+            for p in pid:
+                if 'Wechat' in psutil.Process(p).name():
+                    logging.error("Please close wechat!")
+        except psutil.AccessDenied:
+            logging.error("Please close wechat!")
 
     def take_path(self):
         walk = []
@@ -48,10 +54,10 @@ class Framework:
         for us in walk[1]:
             if us == 'All Users' or us == 'Applet':
                 continue
-            users.append(os.path.join(self.docpath,us))
+            users.append(os.path.join(self.docpath, us))
             self.user.append(us)
-            print('切入了新的历史用户：%s ，它的路径：%s'%(us,os.path.join(self.docpath,us)))
-        
+            logging.info("New user:%s" % us)
+
         self.user_path = users
         self.clean_rubbish()
 
@@ -59,38 +65,37 @@ class Framework:
         for l in self.user_path:
             try:
                 for ll in self.rubbish_doc['Rubbish']:
-                    shutil.rmtree(os.path.join(l,ll))
-                    print('已删除 %s'%ll)
-            except (OSError,FileNotFoundError):
+                    shutil.rmtree(os.path.join(l, ll))
+                    logging.info("Clean:%s" % ll)
+            except (OSError, FileNotFoundError):
                 pass
         self.move_to_temp()
 
     def move_to_temp(self):
-        for l,u in zip(self.user_path,self.user):
+        for l, u in zip(self.user_path, self.user):
             try:
                 for ll in self.rubbish_doc['Documents']:
-                    shutil.move(os.path.join(l,ll),os.path.join(self.temp_path,u,ll))
-                    print('已把 %s 移到 %s'%(os.path.join(l,ll),os.path.join(self.temp_path,u)))
-            except (OSError,FileNotFoundError):
+                    shutil.move(os.path.join(l, ll), os.path.join(self.temp_path, u, ll))
+                    logging.info("Move %s" % os.path.join(l, ll))
+            except (OSError, FileNotFoundError):
                 pass
         self.temp_to_zip()
 
     def temp_to_zip(self):
         os.chdir(self.docpath)
-        with zipfile.ZipFile(self.zip_here,'w') as f:
-            for pn,spn,fn in os.walk('.'):
+        with zipfile.ZipFile(self.zip_here, 'w') as f:
+            for pn, spn, fn in os.walk('.'):
                 for i in fn:
-                    f.write(os.path.join(pn,i))
-                    print('向%s写入：%s'%(self.zip_here,os.path.join(pn,i)))
+                    f.write(os.path.join(pn, i))
+                    logging.info("Write %s" % os.path.join(pn, i))
         self.__del__()
-        
+
     def __del__(self):
         os.chdir(self.app_path)
         try:
             shutil.rmtree(self.docpath)
-            print('已清除完Wechat Files，正在清除临时文件夹……')
+            logging.info("Clean temp......")
             self.temp.cleanup()
-            print('已清除临时文件夹！')
-            print('CleanWechatFiles Framework已结束！')
-        except:
+            logging.info("All done!")
+        except Exception:
             pass
